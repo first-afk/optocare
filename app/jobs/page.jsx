ffe3f46fc132c24338a1bcbd88cac57581440641
Esther Orieji <oriejiestherr@gmail.com>
2026-05-13 00:18:29 +0100

    implements fetching jobs from database

--- app/jobs/page.jsx
+++ app/jobs/page.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
+import { getAllJobs } from "@/lib/actions/jobs.actions";
+import React from "react";
+import SearchInput from "../components/SearchInput";
+import JobFilter from "../components/JobFilter";
 
-const jobsPage = () => {
+const jobsPage = async ({ searchParams }) => {
+  const filters = await searchParams;
+  const title = filters.title ? filters.title : "";
+  const clinic_name = filters.clinic_name ? filters.clinic_name : "";
+
+  const jobs = await getAllJobs({ title, clinic_name });
+  console.log(jobs);
+
   return (
-    <section className='job-section'>
-      <aside className='filter'>
-        
+    <section className="job-section">
+      <div className="title heading-h2">Job Library</div>
+      <aside className="filter">
+        <SearchInput />
+        <JobFilter />
       </aside>
-      <div className='title'></div>
-      <div className='job-grid'></div>
+      <div className="job-grid">
+        {/* {jobs.map((job) => (
+          
+        ))} */}
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default jobsPage
+export default jobsPage;
